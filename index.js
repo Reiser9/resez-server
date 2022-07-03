6c1415faa145645716d5aff44a942b9b1b9a5c03
Reiser9 <63304397+Reiser9@users.noreply.github.com>
2022-07-03 21:48:03 +0500

    'fix'

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { register, login, getMe } from './controllers/UserController.js';
 import validationErrors from './utils/validationErrors.js';
 
 mongoose
-    .connect('mongodb+srv://Reiser95:Google222@cluster0.ztcu1.mongodb.net/resez?retryWrites=true&w=majority')
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log('DB ok'))
     .catch((err) => console.log('DB error', err));
 
@@ -21,7 +21,7 @@ app.get('/auth/me', checkAuth, getMe);
 app.post('/auth/login', loginValidation, validationErrors, login);
 app.post('/auth/register', registerValidation, validationErrors, register);
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
     if(err){
         return console.log(err);
     }
